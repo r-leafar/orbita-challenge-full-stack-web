a501c2fc11eb7d9806558d38ce9c3f5bcfa7ff61
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 20:35:23 -0300

    Adicionado classe Base que definira o tipo do ID das entidades.

---
+++ backend/EdTech/EdTech.Core/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Core.Entities
+{
+    public abstract class BaseEntity<TId>
+    {
+        public TId Id { get; private set; } = default!;
+
+        protected void SetId(TId id)
+        {
+            Id = id;
+        }
+
+    }
+}
