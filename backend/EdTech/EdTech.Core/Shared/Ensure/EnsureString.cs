1e29a06ff2658a188b8e7b40522845397788e490
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 20:02:00 -0300

    Adicionado validações de string compatilhado.

---
+++ backend/EdTech/EdTech.Core/Shared/Ensure/EnsureString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Core.Shared.Ensure
+{
+    public static partial class Ensure
+    {
+
+        public static void NotNullOrWhiteSpace(string? value, [CallerArgumentExpression("value")] string? paramName = null)
+        {
+            NotNull(value, paramName);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("O valor não pode ser vazio.", paramName ?? nameof(value));
+            }
+        }
+        public static void NotNull(string? value, [CallerArgumentExpression("value")] string? paramName = null)
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(paramName ?? nameof(value), "O valor não pode ser nulo.");
+            }
+        }
+    }
+}
