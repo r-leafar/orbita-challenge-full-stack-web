53c0727c93b819fe7d89334e74c31576605a7d8a
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 19:55:05 -0300

    Enum usado para implementação INationalIdentifier

---
+++ backend/EdTech/EdTech.Core/Enums/NationalIdentifierType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Core.Enums
+{
+    public enum NationalIdentifierType
+    {
+        CPF
+    }
+}
