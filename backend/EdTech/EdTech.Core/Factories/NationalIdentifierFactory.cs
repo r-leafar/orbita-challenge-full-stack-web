12cd0e13c0a28fe0626cf715047ce1317d2a9df5
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 20:12:53 -0300

    Criado uma factory para INationalIdentifier

---
+++ backend/EdTech/EdTech.Core/Factories/NationalIdentifierFactory.cs
@@ -0,0 +1,22 @@
+﻿using EdTech.Core.Interfaces;
+using EdTech.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Core.Factories
+{
+    public static class NationalIdentifierFactory
+    {
+        public static INationalIdentifier Create(string type, string value)
+        {
+            return type.ToLower() switch
+            {
+                "cpf" => new CpfIdentifier(value),
+                _ => throw new NotSupportedException($"O identificador '{type}' não é suportado.")
+            };
+        }
+    }
+}
