e98da948c1d8030962076b230fe5851e8ba46060
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 19:53:42 -0300

    Adicionado INationalIdentifier interface para tratamento de documentos genéricos

---
+++ backend/EdTech/EdTech.Core/Interfaces/INationalIdentifier.cs
@@ -0,0 +1,16 @@
+﻿using EdTech.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Core.Interfaces
+{
+    public interface INationalIdentifier
+    {
+        string Number { get; }
+        NationalIdentifierType Type { get; }
+        bool IsValid();
+    }
+}
