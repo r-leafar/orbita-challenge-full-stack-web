8d1d402d8e50841ae6b48116cb2bafed7045e85b
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 19:54:34 -0300

    Criado CpfIdentifier implementação de INationalIdentifier

---
+++ backend/EdTech/EdTech.Core/ValueObjects/CpfIdentifier.cs
@@ -0,0 +1,26 @@
+﻿using EdTech.Core.Enums;
+using EdTech.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Core.ValueObjects
+{
+    public class CpfIdentifier : INationalIdentifier
+    {
+        public string Number { get; }
+        public NationalIdentifierType Type => NationalIdentifierType.CPF;
+
+        public CpfIdentifier(string number)
+        {
+            Number = number;
+        }
+
+        public bool IsValid()
+        {
+            return Number?.Length == 11;
+        }
+    }
+}
