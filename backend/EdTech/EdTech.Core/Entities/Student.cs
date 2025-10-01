12fa0e18e7a31dd2a2ef5b33759b431d6c4c1a0f
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 19:55:42 -0300

    Criação da entidade Student

---
+++ backend/EdTech/EdTech.Core/Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿using EdTech.Core.Interfaces;
+using EdTech.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Core.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; private set; }
+        public string Name{ get; set; }
+        public string Email { get; set; }
+        public string SchoolId { get; set; }
+        public INationalIdentifier NationalIdentifier { get; set; }
+
+        public Student(string schoolId, INationalIdentifier nationalIdentifier)
+        {
+            Id = Guid.NewGuid();
+            SchoolId = schoolId;
+            NationalIdentifier = nationalIdentifier;
+        }
+    }
+}
