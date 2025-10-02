4c00efb312b7ef635440b581ea93dfb73771f1b5
Rafael Saboia <r-leafar@hotmail.com>
2025-10-02 00:39:51 -0300

    Adicionado lib para rodar as migrações.

---
+++ backend/EdTech/EdTech.Infrastructure/Migrations/20251002033215_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EdTech.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    NationalIdValue = table.Column<string>(type: "varchar(20)", nullable: false),
+                    NationalIdType = table.Column<string>(type: "varchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_students", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "students");
+        }
+    }
+}
--- backend/EdTech/EdTech.Infrastructure/Persistence/Configurations/StudentConfiguration.cs
+++ backend/EdTech/EdTech.Infrastructure/Persistence/Configurations/StudentConfiguration.cs
@@ -1,9 +1,11 @@
 ﻿using EdTech.Core.Entities;
+using EdTech.Core.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +16,21 @@ namespace EdTech.Infrastructure.Persistence.Configurations
         public void Configure(EntityTypeBuilder<Student> builder)
         {
             builder.HasKey(x => x.Id);
+
+
+            builder.OwnsOne(s => s.NationalIdentifier, ni =>
+            {
+                ni.Property(n => n.Type)
+                  .HasColumnType("varchar(20)")
+                  .HasColumnName("NationalIdType")
+                  .IsRequired();
+
+                ni.Property(n => n.Number)
+                  .HasColumnType("varchar(20)")
+                  .HasColumnName("NationalIdValue")
+                  .IsRequired();
+            });
+
         }
     }
 }
