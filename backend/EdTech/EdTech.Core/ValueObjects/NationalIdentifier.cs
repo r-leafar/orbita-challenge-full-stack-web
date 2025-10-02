f805c74f68a1268081b2f84039d4f0bd0eb30bbb
Rafael Saboia <r-leafar@hotmail.com>
2025-10-02 00:02:05 -0300

    Transformado interface em classe abstrata.

---
+++ backend/EdTech/EdTech.Core/ValueObjects/NationalIdentifier.cs
@@ -0,0 +1,22 @@
+﻿using EdTech.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Core.ValueObjects
+{
+    public abstract class NationalIdentifier
+    {
+        public string Number { get; init; }
+        public NationalIdentifierType Type { get; init; }
+        protected NationalIdentifier(string number, NationalIdentifierType type)
+        {
+            Number = number;
+            Type = type;
+        }
+        protected abstract bool IsValid();
+
+    }
+}
