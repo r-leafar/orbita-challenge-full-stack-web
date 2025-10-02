9f1109c3207cf90224d19fbebc72c3a4fdfefe66
Rafael Saboia <r-leafar@hotmail.com>
2025-10-02 16:59:38 -0300

    criado script de inicialização do postgres para criação da extensão "uuid-ossp".

---
+++ init-db/init-uuid.sql
@@ -0,0 +1,3 @@
+-- Este comando cria a extensão uuid-ossp (a mais comum para gerar UUIDs)
+-- O comando CREATE EXTENSION só é executado se a extensão ainda não existir.
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
