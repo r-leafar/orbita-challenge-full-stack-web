eaf1c8d72f69fa2afdd18cb7a5b79de40200c35b
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 21:12:33 -0300

    Renomeado UseCase.

---
+++ backend/EdTech/EdTech.Application/UseCases/CreateStudentCommand.cs
@@ -0,0 +1,48 @@
+﻿using EdTech.Application.Dtos;
+using EdTech.Core.Entities;
+using EdTech.Core.Enums;
+using EdTech.Core.Factories;
+using EdTech.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Application.UseCases
+{
+    public class CreateStudentCommand
+    {
+        private readonly IStudentRepository _repository;
+
+
+        public CreateStudentCommand(IStudentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Guid> Handle(CreateStudentRequest dto)
+        {
+            NationalIdentifierType enumNationalIdentifier = NationalIdentifierType.NONE;
+
+           Enum.TryParse<NationalIdentifierType>(dto.NationalIdType,ignoreCase: true, out enumNationalIdentifier);
+
+            if (enumNationalIdentifier == NationalIdentifierType.NONE)
+            {
+                throw new ArgumentException("O tipo do indentificador informado é inválido.");
+            }
+
+            var student = new Student(
+                   name: dto.Name,
+                   email: dto.Email,
+                   schoolId: dto.SchoolId,
+                   nationalIdentifier : NationalIdentifierFactory.Create(enumNationalIdentifier, dto.NationalIdValue)
+               );
+
+            await _repository.AddAsync(student);
+
+            return student.Id;
+        }
+
+    }
+}
