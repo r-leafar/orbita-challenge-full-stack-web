1e1c0afbed7dab16d7a8e51a78830c41a025ad8e
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 22:40:33 -0300

    Cria o método de extensão `MapStudentEndpoints` para lidar com as requisições de Student.

---
+++ backend/EdTech/EdTech.WebApi/Endpoints/StudentEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using EdTech.Application.Dtos;
+using EdTech.Application.UseCases;
+using EdTech.Core.Entities;
+using EdTech.Core.Interfaces.Repositories;
+
+namespace EdTech.WebApi.Endpoints
+{
+    public static class StudentEndpoints
+    {
+        public static RouteGroupBuilder MapStudentEndpoints(this RouteGroupBuilder group)
+        {
+            group.MapPost("/", async (CreateStudentRequest student, IStudentRepository repository) =>
+            {
+                var command =  new CreateStudentCommand(repository);
+                var idStudent = await command.Handle(student);
+
+                return Results.Created($"/students/{idStudent}", student);
+            }).
+            WithName("CreateProduct").
+           Produces<CreateStudentRequest>(StatusCodes.Status201Created).
+           Produces(StatusCodes.Status400BadRequest);
+
+            return group;
+        }
+    }
+}
