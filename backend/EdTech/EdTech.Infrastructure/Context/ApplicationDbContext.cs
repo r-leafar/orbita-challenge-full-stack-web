778e018e7c844ad365a4be367d36d69a6b02f1f3
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 21:41:31 -0300

    Implementa a classe de contexto para o EF Core.

---
+++ backend/EdTech/EdTech.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EdTech.Core.Entities;
+using EdTech.Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
