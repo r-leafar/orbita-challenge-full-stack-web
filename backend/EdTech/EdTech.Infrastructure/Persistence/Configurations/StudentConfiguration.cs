6c12354f17e86dd92d02de2c69617fabaf5d801c
Rafael Saboia <r-leafar@hotmail.com>
2025-10-01 21:40:06 -0300

    Criação de configuração de mapeamento do Student.

---
+++ backend/EdTech/EdTech.Infrastructure/Persistence/Configurations/StudentConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EdTech.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Infrastructure.Persistence.Configurations
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
