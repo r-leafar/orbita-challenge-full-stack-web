ff787286c9b3a6bf7edd094baae5d60e66c77b34
Rafael Saboia <r-leafar@hotmail.com>
2025-10-02 23:27:26 -0300

    Criada nova Extension.

---
+++ backend/EdTech/EdTech.Application/Mappings/StudentExtensions.cs
@@ -0,0 +1,25 @@
+﻿using EdTech.Application.Dtos.Requests;
+using EdTech.Application.Dtos.Responses;
+using EdTech.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Application.Mappings
+{
+    public static class StudentExtensions
+    {
+        public static StudentResponse ToResponse(this Student student)
+        {
+            return new StudentResponse(
+                student.Id,
+                student.Name,
+                student.Email,
+                student.SchoolId,
+                student.NationalIdentifier.Number
+            );
+        }
+    }
+}
