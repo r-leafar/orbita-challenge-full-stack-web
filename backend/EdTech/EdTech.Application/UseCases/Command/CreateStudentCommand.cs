5e01ece041c7da858dd449d8546ef36cd844b5f3
Rafael Saboia <r-leafar@hotmail.com>
2025-10-03 19:27:26 -0300

    Colocado validação em helper.

--- backend/EdTech/EdTech.Application/UseCases/Command/CreateStudentCommand.cs
+++ backend/EdTech/EdTech.Application/UseCases/Command/CreateStudentCommand.cs
@@ -1,5 +1,6 @@
 ﻿using EdTech.Application.Dtos.Requests;
 using EdTech.Application.Exceptions;
+using EdTech.Application.Validation;
 using EdTech.Core.Entities;
 using EdTech.Core.Enums;
 using EdTech.Core.Factories;
@@ -19,14 +20,7 @@ namespace EdTech.Application.UseCases.Command
 
         public async Task<Guid> Handle(CreateStudentRequest dto)
         {
-            NationalIdentifierType enumNationalIdentifier = NationalIdentifierType.NONE;
-
-           Enum.TryParse(dto.NationalIdType,ignoreCase: true, out enumNationalIdentifier);
-
-            if (enumNationalIdentifier == NationalIdentifierType.NONE)
-            {
-                throw new Exceptions.ApplicationException("O tipo do indentificador informado é inválido.", nameof(dto.NationalIdType));
-            }
+            var enumNationalIdentifier = NationalIdentifierValidation.Validate(dto.NationalIdType);
 
             var student = new Student(
                    name: dto.Name,
---
+++ backend/EdTech/EdTech.Application/Validation/NationalIdentifierValidation.cs
@@ -0,0 +1,33 @@
+﻿using EdTech.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTech.Application.Validation
+{
+    public static class NationalIdentifierValidation
+    {
+        public static NationalIdentifierType Validate(string nationalIdType)
+        {
+            if (string.IsNullOrWhiteSpace(nationalIdType))
+            {
+                throw new Exceptions.ApplicationException(
+                    "O tipo do identificador não pode ser vazio.",
+                    nameof(nationalIdType));
+            }
+
+            if (!Enum.TryParse<NationalIdentifierType>(nationalIdType, ignoreCase: true, out var result) ||
+                result == NationalIdentifierType.NONE)
+            {
+                throw new Exceptions.ApplicationException(
+                    $"O tipo de identificador '{nationalIdType}' é inválido.",
+                    nameof(nationalIdType));
+            }
+
+            return result;
+        }
+    }
+
+}
