3425eb87860b6d67fc5dec2bc5fefc17505a8c36
Rafael Saboia <r-leafar@hotmail.com>
2025-10-03 19:01:09 -0300

    Atualização dos testes.

--- backend/EdTech/EdTech.UnitTest/Application/Command/GetStudentPagedQueryTest.cs
+++ backend/EdTech/EdTech.UnitTest/Application/Command/GetStudentPagedQueryTest.cs
@@ -1,8 +1,10 @@
-﻿using EdTech.Application.UseCases.Query;
+﻿using EdTech.Application.Exceptions;
+using EdTech.Application.UseCases.Query;
 using EdTech.Core.Entities;
 using EdTech.Core.Interfaces.Repositories;
 using EdTech.UnitTest.Helpers;
 using Moq;
+using System.Linq.Expressions;
 
 namespace EdTech.UnitTest.Application.Command
 {
@@ -28,27 +30,69 @@ namespace EdTech.UnitTest.Application.Command
 
             var result = await _query.Handle(1, 10);
 
+            _repositoryMock.Verify(r => r.GetPaged(
+                page,
+                pageSize,
+                It.IsAny<Expression<Func<Student, object>>>(), 
+                It.IsAny<bool>(),                              
+                It.IsAny<Expression<Func<Student, object>>[]>()
+            ),
+            Times.Once);
+
             Assert.Equal(pageSize, result.PageSize);
             Assert.Equal(studentList.Count, result.TotalRecords);
             Assert.Equal(page, result.PageNumber);
         }
 
-        //[Fact]
-        //public async Task GetStudentPagedQuery_WithError_ShouldThrowApplicationException()
-        //{
+        [Fact]
+        public async Task GetStudentPagedQuery_WithError_ShouldThrowApplicationException()
+        {
+            int page = 1;
+            int pageSize = 10;
 
+            _repositoryMock.Setup(r => r.GetPaged(page, pageSize, x => x.Name, true, x => x.NationalIdentifier)).ThrowsAsync(new EdTech.Application.Exceptions.ApplicationException(""));
 
-        //    var exception = await Assert.ThrowsAsync<EdTech.Application.Exceptions.ApplicationException>(async () =>
-        //    {
-        //        var result = await _query.Handle(1, 10);
+            var exception = await Assert.ThrowsAsync<EdTech.Application.Exceptions.ApplicationException>(async () =>
+            {
+                var result = await _query.Handle(1, 10);
 
-        //    });
-        //   // _repositoryMock.Setup(r => r.GetPaged(page, pageSize, x => x.Name, true, x => x.NationalIdentifier)).ReturnsAsync(studentList);
+            });
 
+            _repositoryMock.Verify(r => r.GetPaged(
+                page,
+                pageSize,
+                It.IsAny<Expression<Func<Student, object>>>(),
+                It.IsAny<bool>(),
+                It.IsAny<Expression<Func<Student, object>>[]>()
+            ),
+            Times.Once);
 
-        //    Assert.Equal(pageSize, result.PageSize);
-        //    Assert.Equal(studentList.Count, result.TotalRecords);
-        //    Assert.Equal(page, result.PageNumber);
-        //}
+            Assert.Equal("Erro ao consultar o estudante.", exception.Message);
+        }
+        [Fact]
+        public async Task GetStudentPagedQuery_WithError_ShouldThrowException()
+        {
+            int page = 1;
+            int pageSize = 10;
+
+            _repositoryMock.Setup(r => r.GetPaged(page, pageSize, x => x.Name, true, x => x.NationalIdentifier)).ThrowsAsync(new Exception());
+
+            var exception = await Assert.ThrowsAsync<Exception>(async () =>
+            {
+                var result = await _query.Handle(1, 10);
+
+            });
+
+            _repositoryMock.Verify(r => r.GetPaged(
+                page,
+                pageSize,
+                It.IsAny<Expression<Func<Student, object>>>(),
+                It.IsAny<bool>(),
+                It.IsAny<Expression<Func<Student, object>>[]>()
+            ),
+            Times.Once);
+
+            Assert.Equal("Exception of type 'System.Exception' was thrown.", exception.Message);
+        }
     }
 }
