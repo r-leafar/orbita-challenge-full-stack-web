b445a32cd842e96f4f717f484139323840a78093
Rafael Saboia <r-leafar@hotmail.com>
2025-10-03 18:09:37 -0300

    Criado novo cenario de teste.

--- backend/EdTech/EdTech.UnitTest/Application/Command/GetStudentByIdQueryTest.cs
+++ backend/EdTech/EdTech.UnitTest/Application/Command/GetStudentByIdQueryTest.cs
@@ -51,5 +51,17 @@ namespace EdTech.UnitTest.Application.Command
             Assert.Equal("Erro ao consultar o estudante.", exception.Message);
             Assert.Equal("Student com Id 0199abd8-229a-71e7-9d7c-5ddda3f116dd não encontrado.", exception?.InnerException?.Message);
         }
+        [Fact]
+        public async Task GetStudentByIdQuery_WithError_ShouldThrowException()
+        {
+            var id = Guid.Parse("0199abd8-229a-71e7-9d7c-5ddda3f116dd");
+            _repositoryMock.Setup(r => r.GetByIdAsync(id, x => x.NationalIdentifier)).ThrowsAsync(new Exception("Database error"));
+            var exception = await Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await _query.Handle(id);
+            });
+
+            Assert.Equal("Exception of type 'System.Exception' was thrown.",exception.Message);
+        }
     }
 }
