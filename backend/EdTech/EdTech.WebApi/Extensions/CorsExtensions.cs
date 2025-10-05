f1b319c7064b49e63cac01b950e5032ef7a57502
Rafael Saboia <r-leafar@hotmail.com>
2025-10-05 13:48:22 -0300

    ajusta variveis de ambiente no qual é configurado o CORS.

--- backend/EdTech/EdTech.WebApi/Extensions/CorsExtensions.cs
+++ backend/EdTech/EdTech.WebApi/Extensions/CorsExtensions.cs
@@ -4,11 +4,15 @@
     {
         public static IServiceCollection AddFrontendCorsPolicy(this IServiceCollection services, IConfiguration configuration)
         {
+            var clientBaseUrl = configuration["VITE_CLIENT_URL"] ?? "http://localhost";
+            var clientPort = configuration["VITE_PORT"] ?? "3000";
+            var clientUrl = $"{clientBaseUrl}:{clientPort}";
+
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowFrontend",
                     policy =>
-                    {         policy.WithOrigins(configuration["FRONTEND_URL"] ?? "http://localhost:3000")
+                    {         policy.WithOrigins(clientUrl)
                               .AllowAnyHeader()
                               .AllowAnyMethod()
                               .AllowCredentials();
